82fdea6460e5fab8296b52644728746b850a7dd4
May X <17maxiao@gmail.com>
2020-12-02 16:19:55 -0500

    updated twitter model

--- tweet/models.py
+++ tweet/models.py
@@ -2,5 +2,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    creator = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
     text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.DecimalField(max_digits=6, decimal_places=0)
--- twitter/settings.py
+++ twitter/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'profilepage',
     'splash',
     'tweet',
-    'hashtag',
 ]
 
 MIDDLEWARE = [
